6872990a40412d0e3454cc88c0fd99e275f5cd13
Laauraartemis <Lauraartemis04@gmail.com>
2026-09-24 12:42:01 -0300

    Add files via upload

---
+++ cordosemaforo.py
@@ -0,0 +1,7 @@
+cor_do_semaforo = input("qual a cor do semaforo? ")
+if cor_do_semaforo =="verde":
+    print("siga em frente")
+elif cor_do_semaforo =="amarelo":
+    print("atenção")
+else:
+    print("espere o sinal ficar verde")
