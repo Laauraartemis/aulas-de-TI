01165e73b3dbb6cc175c47c5a52db5648e5170f5
Laauraartemis <Lauraartemis04@gmail.com>
2026-09-24 13:02:10 -0300

    Add files via upload

---
+++ login.py
@@ -0,0 +1,9 @@
+nome = input("adicione seu nome: ")
+senha = input("adicione a senha:")
+if nome =="zezinho":
+    print("loging correto")
+elif senha =="gangster":
+    print("senha correta")
+else:
+    print("senha incorreta")
+
